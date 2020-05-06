28af210f15a4cd3138ee4db94be5c44d68232df7
pankaj4653 <pankaj.k4653@gmail.com>
2020-05-06 11:26:13 +0530

    Update : exception_handling file changed

--- exception_handling.py
+++ exception_handling.py
@@ -1,10 +1,14 @@
 try :
 	f = open('file.txt','rb')
-	var = new_var
+	#var = new_var
 except FileNotFoundError as f:
 	#print(f)
 	print("Sorry! file is not found") #custom output for the exception
 except Exception as e:
  	#print(e)
  	print("Sorry, the variable is not found")
-
+else : #Runs when try block does not raise an exception
+ 	print(f.read())
+ 	f.close()
+finally : #runs regardless of what happened in try block, It is executed to free up the resources
+	print("This is finally block which runs regardless of what happened in try block")
