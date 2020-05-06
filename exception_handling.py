bc088dec566d400201289db4db7f3f26367e6a82
pankaj4653 <pankaj.k4653@gmail.com>
2020-05-06 10:22:52 +0530

    Update :  File updated

--- exception_handling.py
+++ exception_handling.py
@@ -1,4 +1,6 @@
 try :
-	f = open('file.txt','rb')
-except Exception:
-	print("Sorry, Not able yo find the file")
+	f = open('file_.txt','rb')
+except FileNotFoundError as e:
+	print("Sorry! file is not found") #custom output for the exception
+except Exception as e:
+ 	print(e)
