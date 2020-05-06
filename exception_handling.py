ef9d67ead7a26c31f28b902bd43a7234057b4ed4
pankaj4653 <pankaj.k4653@gmail.com>
2020-05-06 10:33:43 +0530

    Update : exception_handling file

--- exception_handling.py
+++ exception_handling.py
@@ -1,6 +1,10 @@
 try :
-	f = open('file_.txt','rb')
-except FileNotFoundError as e:
+	f = open('file.txt','rb')
+	var = new_var
+except FileNotFoundError as f:
+	#print(f)
 	print("Sorry! file is not found") #custom output for the exception
 except Exception as e:
- 	print(e)
+ 	#print(e)
+ 	print("Sorry, the variable is not found")
+
